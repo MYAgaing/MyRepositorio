454014aba7e8e392dd6d8927da516aac6252f2b5
MYAgaing <75101772+MYAgaing@users.noreply.github.com>
2024-11-15 15:29:38 +0100

    .

--- Visual/Programación/src/Tema3/Ejercicio19.java
+++ Visual/Programación/src/Tema3/Ejercicio19.java
@@ -6,6 +6,9 @@ public class Ejercicio19 {
 
 	public static void main(String[] args) {
 		Random rm = new Random();
-		String[] palabras = new String[10]("azulejo", "tenedor", "saltamontes", "carretilla", "molinero","sofisticado","terremoto","culinario","teclado","primavera"};
+		String[] palabras = new String[]{"azulejo", "tenedor", "saltamontes", "carretilla", "molinero","sofisticado","terremoto","culinario","teclado","primavera"};
+		int aleatorio = rm.nextInt(0,9);
+		String[] palabra = palabras[aleatorio].split("");
+		
 	}
-
+}
