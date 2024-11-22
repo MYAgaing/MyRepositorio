022804b7cbaa71354184410c8f9dc192b382da06
MYAgaing <75101772+MYAgaing@users.noreply.github.com>
2024-11-22 20:42:49 +0100

    .

---
+++ Visual/Programación/src/Tema3/Ejercicios_avanzados_3.java
@@ -0,0 +1,26 @@
+package Tema3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicios_avanzados_3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rm = new Random();
+		/*
+		 * int aleatorio = rm.nextInt(1,7); int aleatorio2 = rm.nextInt(1,7);
+		 * System.out.println("Cuantas veces quieres tirar?"); int tiradas =
+		 * sc.nextInt(); for (int i = 0; i < tiradas; i++) {
+		 * System.out.println("El primer dado saca  un " + aleatorio);
+		 * System.out.println("El segundo dado saca  un " + aleatorio2); }
+		 */
+		
+		//Ejercicio2
+		
+		String[][] enRaya = new String[3][3];
+		
+		sc.close();
+	}
+
+}
