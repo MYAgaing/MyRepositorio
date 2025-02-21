a5896e1f65363b95b91a9510b05e0326142b32e7
MYAgaing <75101772+MYAgaing@users.noreply.github.com>
2025-02-21 20:37:37 +0100

    0

---
+++ Visual/Ejercicios/src/BigDecimal/BigDecimal.java
@@ -0,0 +1,15 @@
+package BigDecimal;
+
+import java.util.Scanner;
+
+public class BigDecimal {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+	Scanner sc = new Scanner(System.in);
+	BigDecimal numero = sc.nextBigDecimal();
+
+}
