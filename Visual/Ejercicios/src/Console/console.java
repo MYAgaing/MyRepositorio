51ff4dff7be50d2f5daeefeb4156c6534a75a39b
MYAgaing <nicolas.adones8@gmail.com>
2025-02-24 12:45:22 +0100

    0

---
+++ Visual/Ejercicios/src/Console/console.java
@@ -0,0 +1,19 @@
+package Console;
+
+import java.math.BigDecimal;
+
+public class console {
+
+	public static void log(String s) {
+		System.out.println(s);
+	}
+
+	public static void log(Integer s) {
+		System.out.println(s);
+	}
+
+	public static void log(BigDecimal s) {
+		System.out.println(s);
+	}
+
+}
---
+++ Visual/Ejercicios/src/Ejercicio51/BigMoma.java
@@ -0,0 +1,36 @@
+package Ejercicio51;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import Console.console;
+
+public class BigMoma {
+
+	public static void main(String[] args) {
+		List<BigDecimal> big = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		BigDecimal x1;
+		for (int i = 0; i < 4; i++) {
+			do {
+				console.log("Digame un numero");
+				Integer numero = sc.nextInt();
+				x1 = new BigDecimal(numero);
+
+				big.add(x1);
+			} while (big.get(i).min(x1).equals(x1));
+		}
+		Integer num = 0;
+		BigDecimal total = new BigDecimal(num);
+		for (BigDecimal bigDecimal : big) {
+			total.add(bigDecimal);
+		}
+		console.log(total);
+		console.log(big.get(0).divide(big.get(1), 3, RoundingMode.HALF_UP));
+		sc.close();
+
+	}
+}
